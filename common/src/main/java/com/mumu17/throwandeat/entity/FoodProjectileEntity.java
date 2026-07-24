52ce0a2b7ec2f8c015b2fba6f7b11204a7909fef
mumu17-git <moonpapa1709@gmail.com>
2026-07-24 14:47:16 +0900

    version 1.0.0-1.21.1

--- common/src/main/java/com/mumu17/throwandeat/entity/FoodProjectileEntity.java
+++ common/src/main/java/com/mumu17/throwandeat/entity/FoodProjectileEntity.java
@@ -5,6 +5,10 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Pig;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.monster.Strider;
+import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.npc.Villager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile;
@@ -41,65 +45,64 @@ public class FoodProjectileEntity extends ThrowableItemProjectile {
 
         if (!this.level().isClientSide) {
             ItemStack itemStack = this.getItem();
-
-            switch (result.getEntity()) {
-                case Player targetPlayer -> {
-                    FoodProperties foodProperties = itemStack.get(DataComponents.FOOD);
-                    if (foodProperties != null) {
-                        targetPlayer.getFoodData().eat(foodProperties.nutrition(), foodProperties.saturation());
-                        for (FoodProperties.PossibleEffect possibleEffect : foodProperties.effects()) {
-                            if (this.random.nextFloat() < possibleEffect.probability()) {
-                                targetPlayer.addEffect(possibleEffect.effect());
-                            }
-                        }
-                        this.level().playSound(
-                                null,
-                                targetPlayer.getX(),
-                                targetPlayer.getY(),
-                                targetPlayer.getZ(),
-                                targetPlayer.getEatingSound(itemStack),
-                                targetPlayer.getSoundSource(),
-                                0.5F,
-                                this.random.nextFloat() * 0.1F + 0.9F
-                        );
-                    }
-                }
-                case Villager targetVillager -> {
-                    healMob(targetVillager, itemStack);
-                }
-                case LivingEntity targetMob -> {
-                    Player thrower = this.getOwner() instanceof Player player ? player : null;
-
-                    if (thrower != null) {
-                        Vec3 originalPos = thrower.position();
-                        boolean actionExecuted = false;
-                        try {
-                            FoodProjectileEntity.isActive = false;
-                            thrower.setPos(this.getX(), this.getY(), this.getZ());
-
-                            InteractionResult interactResult = targetMob.interact(thrower, InteractionHand.MAIN_HAND);
-
-                            if (!interactResult.consumesAction()) {
-                                interactResult = itemStack.interactLivingEntity(thrower, targetMob, InteractionHand.MAIN_HAND);
-                            }
-
-                            if (!interactResult.consumesAction()) {
-                                itemStack.use(this.level(), thrower, InteractionHand.MAIN_HAND);
-                            }
-
-                            actionExecuted = interactResult.consumesAction();
-
-                        } finally {
-                            thrower.setPos(originalPos.x, originalPos.y, originalPos.z);
-                            FoodProjectileEntity.isActive = true;
-                        }
-
-                        if (!actionExecuted) {
-                            this.healMob(targetMob, itemStack);
+            if (result.getEntity() instanceof Player targetPlayer) {
+                FoodProperties foodProperties = itemStack.get(DataComponents.FOOD);
+                if (foodProperties != null) {
+                    targetPlayer.getFoodData().eat(foodProperties.nutrition(), foodProperties.saturation());
+                    for (FoodProperties.PossibleEffect possibleEffect : foodProperties.effects()) {
+                        if (this.random.nextFloat() < possibleEffect.probability()) {
+                            targetPlayer.addEffect(possibleEffect.effect());
                         }
                     }
+                    this.level().playSound(
+                            null,
+                            targetPlayer.getX(),
+                            targetPlayer.getY(),
+                            targetPlayer.getZ(),
+                            targetPlayer.getEatingSound(itemStack),
+                            targetPlayer.getSoundSource(),
+                            0.5F,
+                            this.random.nextFloat() * 0.1F + 0.9F
+                    );
                 }
-                default -> {
+            } else if (result.getEntity() instanceof AbstractVillager targetVillager) {
+                healMob(targetVillager, itemStack);
+            } else if (result.getEntity() instanceof AbstractHorse targetHorse) {
+                healMob(targetHorse, itemStack);
+            } else if (result.getEntity() instanceof Pig targetPig && targetPig.isSaddled()) {
+                healMob(targetPig, itemStack);
+            } else if (result.getEntity() instanceof Strider targetStrider && targetStrider.isSaddled()) {
+                healMob(targetStrider, itemStack);
+            } else if (result.getEntity() instanceof LivingEntity targetMob) {
+                Player thrower = this.getOwner() instanceof Player player ? player : null;
+
+                if (thrower != null) {
+                    Vec3 originalPos = thrower.position();
+                    boolean actionExecuted = false;
+                    try {
+                        FoodProjectileEntity.isActive = false;
+                        thrower.setPos(this.getX(), this.getY(), this.getZ());
+
+                        InteractionResult interactResult = targetMob.interact(thrower, InteractionHand.MAIN_HAND);
+
+                        if (!interactResult.consumesAction()) {
+                            interactResult = itemStack.interactLivingEntity(thrower, targetMob, InteractionHand.MAIN_HAND);
+                        }
+
+                        if (!interactResult.consumesAction()) {
+                            itemStack.use(this.level(), thrower, InteractionHand.MAIN_HAND);
+                        }
+
+                        actionExecuted = interactResult.consumesAction();
+
+                    } finally {
+                        thrower.setPos(originalPos.x, originalPos.y, originalPos.z);
+                        FoodProjectileEntity.isActive = true;
+                    }
+
+                    if (!actionExecuted) {
+                        this.healMob(targetMob, itemStack);
+                    }
                 }
             }
         }
