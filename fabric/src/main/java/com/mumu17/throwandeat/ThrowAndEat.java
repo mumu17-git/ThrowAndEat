14df5fd4e4e717ffcd7c039a175ae80b5cf79c85
mumu17-git <moonpapa1709@gmail.com>
2026-07-24 11:44:26 +0900

    version 1.0.0-1.21.1

--- fabric/src/main/java/com/mumu17/throwandeat/ThrowAndEat.java
+++ fabric/src/main/java/com/mumu17/throwandeat/ThrowAndEat.java
@@ -6,13 +6,6 @@ public class ThrowAndEat implements ModInitializer {
     
     @Override
     public void onInitialize() {
-        
-        // This method is invoked by the Fabric mod loader when it is ready
-        // to load your mod. You can access Fabric and Common code in this
-        // project.
-
-        // Use Fabric to bootstrap the Common mod.
-        com.mumu17.throwandeat.Constants.LOG.info("Hello Fabric world!");
         com.mumu17.throwandeat.CommonClass.init();
     }
 }
